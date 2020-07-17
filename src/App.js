46189353411f03e0a136d22e7bb11c91bdb2bf94
Atharva Dandekar <dandekar.atharva@gmail.com>
2020-07-17 11:23:18 -0700

    CRA + TAILWIND

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
+        <div class="bg-gray-300 h-12"></div>
+        <div class="bg-gray-500 h-12"></div>
+        <div class="bg-gray-300 h-12"></div>
+        <div class="bg-gray-500 h-12"></div>
+        <div class="bg-gray-300 h-12"></div>
+        <div class="bg-gray-500 h-12"></div>
+        <div class="bg-gray-300 h-12"></div>
+        <div class="bg-gray-500 h-12"></div>
+        <div class="bg-gray-300 h-12"></div>
+        <div class="bg-gray-500 h-12"></div>
+        <div class="bg-gray-300 h-12"></div>
+        <div class="bg-gray-500 h-12"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
